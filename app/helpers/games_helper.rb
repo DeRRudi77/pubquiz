a5a49b046467a20c93f90fee8bddb50a7aa03d75
Rutger Gelling <rutger@hackerone.com>
2021-05-26 20:14:18 +0200

    Add all devise templates
    Beautify the games overview page

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,14 @@
 module GamesHelper
+  def game_state_class(state)
+    case state
+    when 'pending_start'
+      'is-info is-light'
+    when 'started'
+      'is-danger is-light'
+    when 'pending_results'
+      'is-warning is-light'
+    when 'finished'
+      'is-success is-light'
+    end
+  end
 end
